993f1d8b0678690976dc67cc85626e27f3582162
AnoshR <anoshrukadikar@gmail.com>
2025-11-20 12:56:45 +0530

    Created a Calculator app created a Dockerfile and containerized the app. Expanded Terraform networking – route tables, IGW association, and security groups

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,6 +23,65 @@ resource "aws_internet_gateway" "igw"{
     vpc_id = aws_vpc.vpc1.id
 }
 
+#Step 4: Creation of Route Table
+#public route table
+
+resource "aws_route_table" "PublicRouteTable" {
+    vpc_id = aws_vpc.vpc1.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+#Step 5: Association of Subnet with Route Table
+resource "aws_route_table_association" "PublicSubnetAssociation" {
+    subnet_id = aws_subnet.PublicSubnet.id
+    route_table_id = aws_route_table.PublicRouteTable.id
+}
+
+#Step 6: Creation of Security Group
+resource "aws_security_group" "mySG"{
+    name = "allow_traffic"
+    description = "Allow inbound traffic on port 22 and 80"
+    vpc_id = aws_vpc.vpc1.id
+
+    tags = {
+        Name = "allow_traffic_sg"
+    }
+}
+#allow inbound rules for SSH and HTTP
+resource "aws_security_group_rule" "allow_ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_ipv4 = aws_vpc.vpc1.cidr_block
+    security_group_id = aws_security_group.mySG.id
+}
+resource "aws_security_group_rule" "allow_http" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_ipv4 = aws_vpc.vpc1.cidr_block
+    security_group_id = aws_security_group.mySG.id
+}
+#allow all outbound traffic  
+resource "aws_security_group_rule" "allow_all_outbound" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_ipv4 = "0.0.0.0/0"
+    security_group_id = aws_security_group.mySG.id  
+}
+
+
+
+
+
+
 
 /*provider "aws"{
     region = "us-east-2"
